4c9e2a713887de4ebf33579e9aa269cd2e2b518e
Suman Yadav <sumanydv615@gmail.com>
2025-11-20 11:49:32 -0600

    connected the application with postgresql database

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -2,9 +2,13 @@ from sqlalchemy import create_engine    # pyright: ignore[reportMissingImports]
 from sqlalchemy.orm import sessionmaker    # pyright: ignore[reportMissingImports]
 from sqlalchemy.ext.declarative import declarative_base  # pyright: ignore[reportMissingImports]
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db"
+## SQLALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db"  # to connect sqlite
+## engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False}) for connect arg is only for sqlite
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:suman123@localhost/TodoApplicationDatabase'  #password@localhiostname/database
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
